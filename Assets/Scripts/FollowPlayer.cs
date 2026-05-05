99f85427deddff3e854be75e5a3641ae411b124e
Deeapk Subramanian <dee.subramanian@gmail.com>
2026-05-05 15:10:52 +0530

    Final Changes

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -11,7 +11,7 @@ public class FollowPlayer : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         transform.position = Player.transform.position + offset;
     }
